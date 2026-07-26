beb07986f3a8ed5e5a96fbd06d3dc3bf914eb780
amalkallepurath <119942253+amalkallepurath@users.noreply.github.com>
2026-07-26 12:32:51 +0530

    jjj

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,57 @@
 import React,{useState} from 'react'
 
 export default function App() {
-const [dob,setDob] = useState('');
+const [dob,setDob] = useState(null);
 const [age,setAge] = useState(0);
 const [nextBirthday,setNextBirthday] = useState(0);
 const [years,setYears] = useState(0);
 const [months,setMonths] = useState(0);
 const [days,setDays] = useState(0);
-
+const[doberror,setdoberror]=useState(false)
 const handleSubmit = (e) => {
+
   e.preventDefault();
-  const birthDate=new Date(String(dob))
+  if(dob===null){
+   alert("jjj")
+   setdoberror(true)
+  }
+  else{
+  setdoberror(false)
+  const birthDate=new Date(dob.toLocaleString())
 
   const today = new Date();
+
   setDays(today.getDate())
   const agecal=(today.getFullYear()-birthDate.getFullYear()); 
   setMonths(today.getMonth()) ;
   setYears(agecal-1)
-  if(birthDate.getMonth()<today.getMonth()){
+ 
+  const birmonth=Number(birthDate.getMonth())+1;
+ 
+  const todaymonth=Number(today.getMonth())+1;
+ 
+  if(birmonth<=todaymonth){
    setAge(agecal)
-   
+   const nextbir=new Date(String(Number((birthDate.getMonth())+1)+"-"+birthDate.getDate()+"-"+(Number(today.getFullYear())+1)))
+   /*
    const nextbir=new Date (String(birthDate.getDate()+"-"+Number((birthDate.getMonth())+1)+"-"+(Number(today.getFullYear())+1)))
-   
-   
+   alert(nextbir)*/
    const nextbirdaydif=nextbir-today;
    const nextbirthdays=Math.floor((nextbirdaydif/1000/60/60/24)+1);
    setNextBirthday(nextbirthdays)
   }
   else{
+    
    setAge(agecal-1)
-   const nextbir=new Date (String(birthDate.getDate()+"-"+Number((birthDate.getMonth())+1)+"-"+today.getFullYear()))
-   
-   
-   const nextbirdaydif=nextbir-today;
+    const nextbirr=new Date(String(Number((birthDate.getMonth())+1)+"-"+birthDate.getDate()+"-"+today.getFullYear()))
+    /*
+   const nextbir=new Date (String(birthDate.getDate()+"-"+Number((birthDate.getMonth())+1)+"-"+today.getFullYear()))*/
+   const nextbirdaydif=nextbirr-today;
    const nextbirthdays=Math.floor((nextbirdaydif/1000/60/60/24)+1);
    setNextBirthday(nextbirthdays)
    
   }
+}
 }
   return (
     <div className='md:bg-indigo-500 bg-white h-[100vh] flex justify-center md:items-center'>
@@ -45,6 +60,7 @@ const handleSubmit = (e) => {
         <h1 className='text-indigo-600 font-semibold text-2xl mt-5 text-center'>Age Calculator</h1>
         <label className='text-gray-500 mt-10 text-sm'>Enter your date of birth</label>
         <input type="date" onChange={(e)=>{setDob(e.target.value)}} className=" mt-2 w-[300px] p-1 text-indigo-500 rounded-md border border-indigo-200 focus:outline-none"/>
+       {doberror? <p className='text-xs text-red-500'>Please Enter DOB</p> : ""}
       <input type="submit" value="Calculate" onClick={handleSubmit} className='bg-indigo-500 text-white mt-6 p-2 rounded-md cursor-pointer hover:bg-indigo-600'/>
     
       <div className="grid grid-cols-3 gap-4 mt-6 ">
